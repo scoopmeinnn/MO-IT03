035030155da5483f7479db239c162a05c7bb0e4e
Renzen1106 <lr.grarboleda@mmdc.mcl.edu.ph>
2025-06-09 20:10:39 +0800

    Add CP2 GUI skeleton, CSV header files, and project structure

---
+++ motorphemployeeapp/src/com/motorph/employeeapp/AppGUI.java
@@ -0,0 +1,8 @@
+// AppGUI.java
+package com.motorph.employeeapp;
+
+public class AppGUI {
+    public static void main(String[] args) {
+        // TODO: show LoginDialog, then the main JTabbedPane
+    }
+}
---
+++ motorphemployeeapp/src/com/motorph/employeeapp/ValidatedLeaveRequest.java
@@ -0,0 +1,5 @@
+package com.motorph.employeeapp;
+
+public class ValidatedLeaveRequest {
+    
+}
